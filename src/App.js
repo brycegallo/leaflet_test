5d35be7b4788a726e84627b1f922d9204f5b3390
bryce <76966016+brycegallo@users.noreply.github.com>
2023-08-24 18:29:37 -0400

    Add Markers

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-// import logo from './logo.svg';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import "./styles.css";
+import "leaflet/dist/leaflet.css"
 import { MapContainer, TileLayer } from "react-leaflet";
 
 export default function App() {
+  // markers
+  const markers = [
+    {
+      geocode: [48.86, 2.3522],
+      popUp: "pop up 1"
+    },
+    {
+      geocode: [48.85, 2.3522],
+      popUp: "pop up 2"
+    },
+    {
+      geocode: [48.855, 2.34],
+      popUp: "pop up 3"
+    },
+  ];
+
   return (
     <MapContainer center={[48.8566, 2.3522]} zoom={13}>
       <TileLayer
