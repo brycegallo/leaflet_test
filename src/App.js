988e1b31e677fafff73fa697393f612b9d3d42f3
bryce <76966016+brycegallo@users.noreply.github.com>
2023-08-24 21:47:35 -0400

    Change Icons/Popups

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import "leaflet/dist/leaflet.css"
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from 'leaflet';
 
 export default function App() {
   // markers
@@ -19,12 +20,25 @@ export default function App() {
     },
   ];
 
+  const customIcon = new Icon({
+    iconUrl: require("./img/icon.png"),
+    iconSize: [38, 38]
+  })
+
   return (
     <MapContainer center={[48.8566, 2.3522]} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {markers.map(marker => (
+        <Marker position={marker.geocode} icon={customIcon}>
+          <Popup>{marker.popUp}</Popup>
+        </Marker>
+      ))
+
+      }
+
     </MapContainer>
   );
 }
